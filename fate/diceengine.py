5185b8298d60a82f33eb57c5e94c1a76f1133d76
shimst3r <shimst3r@gmail.com>
2021-01-03 21:02:16 +0100

    Add more tests and functionality to diceengine

--- fate/diceengine.py
+++ fate/diceengine.py
@@ -2,6 +2,7 @@
 diceengine implements functionality for Fate dice and their arithmetic.
 """
 import enum
+import random
 
 
 class Face(enum.IntEnum):
@@ -10,3 +11,13 @@ class Face(enum.IntEnum):
     BLANK = 0
     MINUS = -1
     PLUS = 1
+
+    def __repr__(self):
+        return self.name.lower()
+
+
+class Die:
+    sides = list(Face)
+
+    def roll(self):
+        return random.choice(self.sides)
